7d05c9fb63736a47cc3c7c5e3bee505947a544cb
小公墓 <xiaogongmuwu@163.com>
2019-03-06 00:58:07 +0800

    1
    last

--- js/canvas-autoDropWater.js
+++ js/canvas-autoDropWater.js
@@ -11,9 +11,10 @@
         canvas = document.getElementById("canvas_water"),
         ctx = canvas.getContext("2d"),
         clickCount = 0,
-        rainCount = 25,        //水滴数量
+        rainCount = 28,        //水滴数量
         mX = -100,
-        mY = -100;
+        mY = -100,
+        waterSize = 12;
         canvas.width = window.innerWidth;
         canvas.height = window.innerHeight;
 
@@ -25,7 +26,7 @@
             if(rain.exist == 1){
                 ctx.beginPath();
                 ctx.moveTo(rain.x,rain.y);
-                ctx.bezierCurveTo(rain.x-20,rain.y+20,rain.x+20,rain.y+20,rain.x,rain.y);
+                ctx.bezierCurveTo(rain.x-rain.size,rain.y+rain.size,rain.x+rain.size,rain.y+rain.size,rain.x,rain.y);
                 ctx.fillStyle = rain.color;
                 ctx.globalAlpha = rain.opacity;
                 ctx.fill();
@@ -37,7 +38,7 @@
 
 
             if (rain.exist == 0) {
-                var d = 5 * (rain.maxRadi - rain.radi) / rain.maxRadi;
+                var d = 1 * (rain.maxRadi - rain.radi) / rain.maxRadi;
                 var r = d / 2;
                 ctx.beginPath();
                 ctx.moveTo(rain.x - rain.radi, rain.dy);
@@ -68,7 +69,7 @@
                 ctx.stroke();
 
                 ctx.globalAlpha = rain.opacity;
-                rain.radi += rain.speed * 0.5;
+                rain.radi += rain.speed * 0.28;
                 if (rain.radi > rain.maxRadi) {
                     rain.exist = -1;
                 }
@@ -87,8 +88,10 @@
         rain.x =  Math.floor(Math.random() * canvas.width),
         rain.y = 0;
         rain.dy = Math.floor(Math.random() * canvas.height * 0.6 + canvas.height * 0.4),
-        rain.speed = (Math.random() * 2) + 5 * rain.dy/canvas.height,
+        rain.speed = (Math.random() * 1) + 5 * rain.dy/canvas.height,
+        rain.size = waterSize*(rain.dy-0.5*canvas.height)/(0.4*canvas.height),
         rain.opacity = (Math.random() * 0.2) + 0.4 * rain.dy/canvas.height;
+        rain.maxRadi= 100*(rain.dy-0.3*canvas.height)/(0.4*canvas.height);
         rain.exist = 1;
         rain.radi = 0;
     }
@@ -96,14 +99,17 @@
     function resetClick(rain,x,y){
         rain.x =  x,
         rain.y = 0;
-        rain.dy = y,
-        rain.speed = (Math.random() * 2) + 5 * rain.dy/canvas.height,
+        rain.dy = y;
+        if (rain.dy<0.4*canvas.height) {rain.dy=0.4*canvas.height}
+        rain.speed = (Math.random() * 1) + 5 * rain.dy/canvas.height,
+        rain.size = waterSize*(rain.dy-0.5*canvas.height)/(0.4*canvas.height),
         rain.opacity = (Math.random() * 0.2) + 0.4 * rain.dy/canvas.height;
+        rain.maxRadi= 100*(rain.dy-0.3*canvas.height)/(0.4*canvas.height);
         rain.exist = 1;
         rain.radi = 0;
-        rain.color = "#DD0303";    //点击水滴的颜色
-        rain.aboveColor =  "#FF0000";
-        rain.underColor = "#AA0000"
+        rain.color = "#0F0";    //点击水滴的颜色
+        rain.aboveColor =  "#0F0";
+        rain.underColor = "#0F0"
     }
 
     function init() {
@@ -111,18 +117,20 @@
             var x = Math.floor(Math.random() * canvas.width),
                 y = - Math.floor(Math.random() * canvas.height),
                 dy = Math.floor(Math.random()  * canvas.height * 0.6 + canvas.height * 0.4),  //落点屏幕0.4以下
-                speed = (Math.random() * 2) + 5 * dy/canvas.height,                                                //下落速度 
-                opacity = (Math.random() * 0.2) + 0.4 * dy/canvas.height;                                          //透明度
-
+                speed = (Math.random() * 1) + 5 * dy/canvas.height,                                                //下落速度 
+                size = waterSize*(dy-0.5*canvas.height)/(0.4*canvas.height),
+                opacity = (Math.random() * 0.2) + 0.4 * dy/canvas.height,                                          //透明度
+                maxRadi= 100*(dy-0.3*canvas.height)/(0.4*canvas.height);
                 rains.push({
                     x: x,
                     y: y,
                     dy: dy,
                     speed: speed,
+                    size: size,
                     opacity: opacity,
                     exist: 1,           //是否在空中
                     radi: 0,         //水花半径
-                    maxRadi: 200,       //最大水花半径
+                    maxRadi: maxRadi,       //最大水花半径
                     color: "#64C8E9", //"RGB(100,200,233)" 自动掉落水滴颜色
                     aboveColor: "#96DEFF",        //"RGB(150,222,255)"
                     underColor: "#4D92E5"               //"RGB(77,146,229)"
